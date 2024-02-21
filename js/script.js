5eb350a3cb9c80bf2dd254ed51cc95a1f9ac0aa1
erickgcia <erick17a@hotmail.com>
2024-02-21 16:59:40 -0600

    feature: Creation of the form search container.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const searchIcon = document.querySelector('.nav-icon')
-
+const searchIcon = document.querySelector('.nav__icon')
+const container = document.querySelector('.container')
 
 searchIcon.addEventListener('click', () => {
-  //toggle search container
+  toggleClass(container, 'container--toggle')
 })
+
+function createElement(el) {
+  const element = document.createElement(el)
+  document.body.append(element)
+  return element
+}
+
+function toggleClass(element, cssClass) {
+  if(!element.classList.contains(cssClass)) {
+    element.classList.add(cssClass)
+  } else {
+    element.classList.remove(cssClass)
+  }
+}
