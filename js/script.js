65605bb8ecfc2eea63e827c192ebd542f8b3843a
erickgcia <erick17a@hotmail.com>
2024-02-22 14:35:39 -0600

    feature: icon interactions with the search input and updates to the container search box.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,6 @@ searchIcon.addEventListener('click', () => {
   toggleClass(container, 'container--toggle')
 })
 
-function createElement(el) {
-  const element = document.createElement(el)
-  document.body.append(element)
-  return element
-}
-
 function toggleClass(element, cssClass) {
   if(!element.classList.contains(cssClass)) {
     element.classList.add(cssClass)
@@ -18,3 +12,49 @@ function toggleClass(element, cssClass) {
     element.classList.remove(cssClass)
   }
 }
+
+const arrayLinks = ['Living Rooms', 'Discounts', 'Reviews']
+
+function listCreator(arr) {
+  const ul = document.createElement('ul')
+  
+  arr.forEach(item => {
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+
+    a.href = `#${item.toLowerCase()}`
+    a.innerText = item;
+    li.appendChild(a)
+    ul.appendChild(li)
+  })
+
+  container.appendChild(ul)
+
+  return ul
+}
+
+const recList = listCreator(arrayLinks)
+recList.classList.add('recommendations')
+
+const recItems = Array.from(recList.children)
+recItems.forEach(item => {
+  item.classList.add('recommendations__item')
+  const recLink = item.firstChild
+  recLink.classList.add('recommendations__link')
+})
+
+const searchInput = document.querySelector('.container__input')
+const removeIcon = document.querySelector('.container__icon--remove')
+
+searchInput.addEventListener('input', () => {
+  if(searchInput.value) {
+    removeIcon.classList.add('icon--show')
+  } else {
+    removeIcon.classList.remove('icon--show')
+  }
+})
+
+removeIcon.addEventListener('click', () => {
+  searchInput.value = ''
+  removeIcon.classList.remove('icon--show')
+})
