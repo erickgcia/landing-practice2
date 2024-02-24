d901087864eb347a83bcb110c4a55316f44436f2
erickgcia <erick17a@hotmail.com>
2024-02-24 14:33:56 -0600

    feature: JS users testimonials slider modifications.

--- js/script.js
+++ js/script.js
@@ -58,3 +58,58 @@ removeIcon.addEventListener('click', () => {
   searchInput.value = ''
   removeIcon.classList.remove('icon--show')
 })
+
+const testimonialSection = document.querySelector('.testimonials__section')
+const testimonialBtns = document.querySelector('.testimonials__slider-btn')
+const testCard = document.querySelector('.card--oversize')
+
+const users = [
+  {
+    id: '002',
+    img: '../img/client-portrait-2.jpg',
+    name: 'Amanda Brooks',
+    tags: 'Architect | Company Name',
+    txt: 'Lorem ipsum...'
+  },
+
+  {
+    id: '003',
+    img: '../img/client-portrait-3.jpg',
+    name: 'Leyla Lee',
+    tags: 'Designer | Company Name',
+    txt: 'Lorem ipsum...'
+  }
+]
+const reviewCard = duplicateElement(testCard, testimonialSection)
+modifyCard(reviewCard,users,'002')
+
+const newReviewCard = duplicateElement(testCard, testimonialSection)
+modifyCard(newReviewCard,users,'003')
+
+function duplicateElement(el, parent) {
+  const newElement = el.cloneNode(true)
+  parent.appendChild(newElement)
+  return newElement
+}
+
+function modifyCard(card, users, id) {
+  const img = card.children[0]
+  const name = card.children[1]
+  const tags = card.children[2]
+  const txt = card.children[3]
+
+  const findUser = users.find(user => user.id === id)
+  
+  if(findUser) {
+    img.src = findUser.img
+    name.innerText = findUser.name
+    tags.innerText = findUser.tags
+    txt.innerText = findUser.txt
+  }
+}
+
+/* testimonialBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    //Slide to the next div()
+  })
+}) */
